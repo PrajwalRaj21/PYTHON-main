099e4d3f62a57f7256e8f28c78d7c66a72c6e840
PrajwalRaj21 <christprajwal@gmail.com>
2023-10-19 23:35:11 +0545

    If statements

---
+++ BASICS/if_statements.py
@@ -0,0 +1,11 @@
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print("u are a male or tall or both")
+elif is_male and not(is_tall):
+    print("What are you exactly?")
+elif is_tall and not(is_male):
+    print("I am so confused rn")
+else:
+    print("You are not tall or a man")

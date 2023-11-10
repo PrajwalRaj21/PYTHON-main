da34f70555e7f01e5c8f11c866874dbee151affb
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-10 18:05:38 +0545

    for loops

---
+++ BASICS/for_loop.py
@@ -0,0 +1,13 @@
+#for loops
+
+friends = ["Jim", "karen", "harry"]
+for friends in friends:
+  print(friends)
+
+
+for index in range(10, 100):
+    print(index)
+
+
+friends = ["Jim", "karen", "harry"]
+print(len(friends))

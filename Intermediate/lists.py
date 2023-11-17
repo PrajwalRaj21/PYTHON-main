6cf5c83ec94bc75d5102a043ba5aecf9e852cdba
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-17 17:30:05 +0545

    intermediate list

---
+++ Intermediate/lists.py
@@ -0,0 +1,12 @@
+mylist= ["banana", "cherry", "apple"]
+
+print(mylist)
+
+mylist2 = list()
+print(mylist2)
+
+mylist3 = [5, True, "possible"]
+print(mylist3)
+
+item = mylist[1]
+print(item)  #prints our cherry

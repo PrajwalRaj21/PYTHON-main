3d6326eac0e88c7ef7bb36fbe93d792e34bc48a9
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-17 08:59:24 +0545

    extracts every Top Gear episode from the wikipedia

---
+++ Automation with python/first.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+top_gear = pd.read_html("https://en.wikipedia.org/wiki/List_of_Top_Gear_(2002_TV_series)_episodes")
+
+print(len(top_gear))
+
+print(top_gear[1])  #extracts the second season data

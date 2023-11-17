a12eae59679fcecb853fd8fff1bda97d2f866a36
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-17 09:12:27 +0545

    CSV extract and rename

--- Automation with python/CSV_scrap.py
+++ Automation with python/CSV_scrap.py
@@ -3,6 +3,6 @@ import pandas as pd
 football_data = pd.read_csv("https://www.football-data.co.uk/mmz4281/2324/E0.csv")
 
 
-football_data.rename(columns={'PCAHA' : "home_goals"}, inplace = True)
+football_data.rename(columns={'PCAHA' : "home_goals"}, inplace = True)  #renames the PCAHA to home_goals
 
 print(football_data)

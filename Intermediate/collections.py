ae2960b15ad20749ccd17fa0f7616cb6380f3297
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-10 20:46:25 +0545

    commit

---
+++ Intermediate/collections.py
@@ -0,0 +1,4 @@
+from collections import Counter
+a = "aaaaaaaabbbcccc"
+my_counter = Counter(a)
+print(my_counter)
---
+++ Intermediate/strings.py
@@ -0,0 +1,13 @@
+my_string = "HelloWorld"
+char = my_string[0] #prints out H
+print(char)
+substring = my_string[1:5]  #prints from e to o
+print(substring)
+new_string = """Multiple
+lines
+are 
+printed"""
+
+print(my_string)
+print(new_string)
+
